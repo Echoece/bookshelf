7882e3d3597b246875c9e5482e353ba42c6d28f7
echoece <echoece@gmail.com>
2022-01-15 00:22:19 +0600

    added book_request model and migration

---
+++ database/migrations/2022_01_14_180858_create_book_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('requested_book');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_requests');
+    }
+}
